e86428a5d22d23b405fe86dcb44dd6a76057d267
RithkoM <49213354+RithkoM@users.noreply.github.com>
2022-03-05 15:55:50 +0300

    Update codes.lua

--- codes.lua
+++ codes.lua
@@ -1,1 +1,96 @@
-
+["tahlilsonuc"] = {"amb@world_human_tourist_map@male@base", "base", "Tahlil Sonuçları", AnimationOptions =
+   {
+       Prop = 'prop_cd_paper_pile1',
+       PropBone = 28422,
+       PropPlacement = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0},
+       EmoteMoving = true,
+       EmoteLoop = true
+   }},
+   ["makas"] = {"anim@amb@clubhouse@tutorial@bkr_tut_ig3@", "machinic_loop_mechandplayer", "Makas", AnimationOptions =
+   {
+       Prop = 'v_serv_bs_scissors',
+       PropBone = 28422,
+       PropPlacement = {0.1, 0.1, 0.0, 0.0, 0.0, 0.0},
+       EmoteMoving = true,
+       EmoteLoop = true
+   }},
+   ["siringa"] = {"anim@amb@clubhouse@tutorial@bkr_tut_ig3@", "machinic_loop_mechandplayer", "Şırınga", AnimationOptions =
+   {
+       Prop = 'prop_syringe_01',
+       PropBone = 28422,
+       PropPlacement = {0.04, 0.04, 0.0, 0.0, 0.0, 0.0},
+       EmoteMoving = true,
+       EmoteLoop = true
+   }},
+   ["kesikcihazi"] = {"anim@amb@clubhouse@tutorial@bkr_tut_ig3@", "machinic_loop_mechandplayer", "Kesik Cihazı", AnimationOptions =
+   {
+       Prop = 'prop_cranial_saw',
+       PropBone = 28422,
+       PropPlacement = {0.04, 0.05, 0.0, 0.0, 180.0, 0.0},
+       EmoteMoving = true,
+       EmoteLoop = true
+   }},
+   ["makas2"] = {"anim@amb@clubhouse@tutorial@bkr_tut_ig3@", "machinic_loop_mechandplayer", "Makas (Zeminde)", AnimationOptions =
+   {
+       Prop = 'v_serv_bs_scissors',
+       PropBone = 28422,
+       PropPlacement = {0.1, 0.1, 0.0, 0.0, 0.0, 0.0},
+       EmoteMoving = false,
+       EmoteLoop = true
+   }},
+   ["siringa2"] = {"anim@amb@clubhouse@tutorial@bkr_tut_ig3@", "machinic_loop_mechandplayer", "Şırınga (Zeminde)", AnimationOptions =
+   {
+       Prop = 'prop_syringe_01',
+       PropBone = 28422,
+       PropPlacement = {0.04, 0.04, 0.0, 0.0, 0.0, 0.0},
+       EmoteMoving = false,
+       EmoteLoop = true
+   }},
+   ["kesikcihazi2"] = {"anim@amb@clubhouse@tutorial@bkr_tut_ig3@", "machinic_loop_mechandplayer", "Kesik Cihazı (Zeminde)", AnimationOptions =
+   {
+       Prop = 'prop_cranial_saw',
+       PropBone = 28422,
+       PropPlacement = {0.04, 0.05, 0.0, 0.0, 180.0, 0.0},
+       EmoteMoving = false,
+       EmoteLoop = true
+   }},
+   ["saglikcantasi"] = {"move_weapon@jerrycan@generic", "idle", "Sağlık Çantası 1", AnimationOptions =
+   {
+       Prop = 'prop_ld_health_pack',
+       PropBone = 28422,
+       PropPlacement = {0.15, 0.0, 0.0, 0.0, -90.0, 90.0},
+       EmoteMoving = true,
+       EmoteLoop = true
+   }},
+   ["saglikcantasi2"] = {"move_weapon@jerrycan@generic", "idle", "Sağlık Çantası 2", AnimationOptions =
+   {
+       Prop = 'xm_prop_x17_bag_med_01a',
+       PropBone = 28422,
+       PropPlacement = {0.37, 0.0, 0.0, 0.0, -90.0, 90.0},
+       EmoteMoving = true,
+       EmoteLoop = true
+   }},
+   ["saglikcihazi"] = {"move_weapon@jerrycan@generic", "idle", "Sağlık Cihazı", AnimationOptions =
+   {
+       Prop = 'prop_ld_case_01',
+       PropBone = 28422,
+       PropPlacement = {0.098, 0.0, 0.0, 0.0, -90.0, 90.0},
+       EmoteMoving = true,
+       EmoteLoop = true
+   }},
+   ["sandalyekapali"] = {"move_weapon@jerrycan@generic", "idle", "Sandalye (Kapalı)", AnimationOptions =
+   {
+       Prop = 'prop_med_bag_01b',
+       PropBone = 28422,
+       PropPlacement = {0.35, 0.3, 0.0, -90.0, 0.0, 0.0},
+       EmoteMoving = true,
+       EmoteLoop = true
+   }},
+   ["sandalyeacik"] = {"anim@heists@box_carry@", "idle", "Sandalye (Açık)", AnimationOptions =
+   {
+       Prop = "prop_med_bag_01",
+       PropBone = 28422,
+       PropPlacement = {-0.0, -0.9, -0.51, -20.0, 0.0, 180.0},
+       EmoteLoop = true,
+       EmoteMoving = true,
+   }},
